2f71156bb172c0b02edd8b41c1c2ef023a7a6e82
Thomas Levine <occurrence@thomaslevine.com>
2013-07-14 20:30:05 -0700

    slopenecessary

--- battleship.hs
+++ battleship.hs
@@ -1,7 +1,7 @@
 battleship1d :: Num n => (n -> n) -> n -> n -> n -> n
 battleship1d f a b prevCenters slopes
   | distanceToCenter < 0.01 = f $ head prevCenters
-  | otherwise = maximum $ battleship1d f a b centers slopes
+  | otherwise = maximum $ map (\c -> battleship1d f a b centers slopes
   where
     center = ((a + b) / 2)
     centers = center:prevCenters
@@ -9,3 +9,14 @@ battleship1d f a b prevCenters slopes
     distanceToCenter = (b - a) / 2
 
 -- battleship f 3 5 []
+
+
+expectedYield :: Num n => (n -> n) -> n -> n -> n -> [n]
+expectedYield f a b highWater slopes = 
+  where
+    planarHeight = ((f a) + (f b)) / 2
+    minSlope = ((f b) - (f a)) / (b - a)
+
+-- Slope necessary to be a new peak
+slopeNecessaryToBeANewPeak :: Num n => (n -> n) -> n -> n -> n
+slopeNecessaryToBeANewPeak f a b high = (high - ((f a) + (f b))/2) / ((b - a) / 2)
