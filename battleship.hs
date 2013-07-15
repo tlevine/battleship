abd1f2f55dfe355e8e80aaa2d350770c00f27a78
Thomas Levine <occurrence@thomaslevine.com>
2013-07-15 10:38:32 -0700

    blah

--- battleship.hs
+++ battleship.hs
@@ -1,21 +1,25 @@
 import qualified Data.Heap as H
 
+type Candidates n = H.MaxPrioHeap n (n, n, Maybe n)
+-- expectedyield (a, b, centerValue)
+
 battleship1d :: Num n => (n -> n) -> n -> n -> n
 battleship1d f a b = battleship1dInternal f a b [] [] []
 
-battleship1dInternal :: Num n => (n -> n) -> n -> n -> [n] -> [n]
+battleship1dInternal :: Num n => (n -> n) -> n -> n -> Candidates n -> [n]
 battleship1dInternal f prevA prevB high prevBounds prevSlopes prevCandidates
   | nextExpectedYield < 10 = high
   | otherwise = battleship1dInternal f a b bounds slopes
   where
     bounds = (prevA,prevB):prevBounds
-    slopes = ():prevSlopes
-    nextCandidate:moreCandidates = candidates f prevA prevB high bounds slopes
-    ((a,b) nextExpectedYield) = nextCandidate
+    rise = 
+    run =
+    slopes = (rise/run):prevSlopes
+    nextCandidates = candidates f prevA prevB high bounds slopes
 
 
-candidates :: Num n => (n -> n) -> n -> n -> [n] -> [n] -> [((n,n), n)]
-candidates f a b high bounds slopes = sortBy (compare `on` snd) $ zip bounds expectedYields
+candidates :: Num n => (n -> n) -> n -> n -> [n] -> [n] -> Candidates n -> Candidates n
+candidates f a b high bounds slopes prevCandidates = 
   where
     expectedYields = map bounds (\(a,b) -> expectedYield f a b high slopes)
 
